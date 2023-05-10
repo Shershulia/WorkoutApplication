483c554cbdea498a5dc543512e343d11ac1abc6e
Shershulia <krol31072002@gmail.com>
2023-05-10 23:29:06 +0200

    Ts formatting

--- frontend/src/calculator/calculatorOfCalories.tsx
+++ frontend/src/calculator/calculatorOfCalories.tsx
@@ -2,20 +2,21 @@ import React, { FC, useState } from 'react';
 import GenderSection from './genderSection';
 
 const CalculatorOfCalories: FC = () => {
-    const [gender, setGender] = useState("M");
+    const [gender, setGender] = useState<string>("M");
+    const [genderIsGiven, setGenderIsGiven] = useState<boolean>(false);
 
-    const handleGenderChange = (message) => {
+    const handleGenderChange = (message: string): void => {
         setGender(message);
         console.log(message);
+        setGenderIsGiven(true);
     }
       
-  return (
-    <div className="flex flex-col items-center justify-center h-screen ">
-        <h1 className="text-3xl font-bold mb-12">Calculator of calories</h1>
-        <GenderSection onMessage={handleGenderChange}></GenderSection>
-    
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold mb-12">Calculator of calories</h1>
+            {!genderIsGiven && <GenderSection onMessage={handleGenderChange} />}
+        </div>
+    );
 };
 
 export default React.memo(CalculatorOfCalories);
