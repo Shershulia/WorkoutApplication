3a324beb86f0ff911897eda3efaf4af68c150afb
Shershulia <krol31072002@gmail.com>
2023-05-10 23:24:50 +0200

    Gender handling

--- frontend/src/calculator/calculatorOfCalories.tsx
+++ frontend/src/calculator/calculatorOfCalories.tsx
@@ -2,12 +2,17 @@ import React, { FC, useState } from 'react';
 import GenderSection from './genderSection';
 
 const CalculatorOfCalories: FC = () => {
+    const [gender, setGender] = useState("M");
 
-
+    const handleGenderChange = (message) => {
+        setGender(message);
+        console.log(message);
+    }
+      
   return (
     <div className="flex flex-col items-center justify-center h-screen ">
         <h1 className="text-3xl font-bold mb-12">Calculator of calories</h1>
-        <GenderSection></GenderSection>
+        <GenderSection onMessage={handleGenderChange}></GenderSection>
     
     </div>
   );
--- frontend/src/calculator/genderSection.tsx
+++ frontend/src/calculator/genderSection.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FC } from "react";
 
-const GenderSection: FC = () => {
+const GenderSection: FC = ({onMessage}) => {
     const [gender, setGender] = useState<string>("M"); //Gender is man by default
 
     function onChangeGenderM(event: React.ChangeEvent<HTMLInputElement>): void {
@@ -9,6 +9,9 @@ const GenderSection: FC = () => {
     }
     function onChangeGenderF(event: React.ChangeEvent<HTMLInputElement>): void {
         setGender("F");
+    }
+    function nextQuestion(event: React.ChangeEvent<HTMLInputElement>): void {
+        onMessage(gender);
     }
 
     return (
@@ -42,7 +45,7 @@ const GenderSection: FC = () => {
                 </label>
                 </div>
                 <div className="flex justify-center">
-                <button className="max-w-xs relative inline-flex items-center justify-center p-0.5 mt-5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white
+                <button onClick={nextQuestion} className="max-w-xs relative inline-flex items-center justify-center p-0.5 mt-5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white
                  dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 ">
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 ">
                         Go to next question
