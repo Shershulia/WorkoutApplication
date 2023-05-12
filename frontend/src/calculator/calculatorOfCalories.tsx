6ad9735d6bcfc9fc293b356823bdbd33ca017f3e
Shershulia <krol31072002@gmail.com>
2023-05-12 17:53:15 +0200

    first commit

--- frontend/src/calculator/calculatorOfCalories.tsx
+++ frontend/src/calculator/calculatorOfCalories.tsx
@@ -2,19 +2,17 @@ import React, { FC, useState } from 'react';
 import GenderSection from './genderSection';
 
 const CalculatorOfCalories: FC = () => {
-    const [gender, setGender] = useState<string>("M");
-    const [genderIsGiven, setGenderIsGiven] = useState<boolean>(false);
+    const [gender, setGender] = useState<string>("");
 
     const handleGenderChange = (message: string): void => {
         setGender(message);
         console.log(message);
-        setGenderIsGiven(true);
     }
       
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-3xl font-bold mb-12">Calculator of calories</h1>
-            {!genderIsGiven && <GenderSection onMessage={handleGenderChange} />}
+            {gender==="" && <GenderSection onMessage={handleGenderChange} />}
         </div>
     );
 };
