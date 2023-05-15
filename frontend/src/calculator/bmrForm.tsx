1a59673c6b561ec8d9aaeaa36be0be4b53108228
Shershulia <krol31072002@gmail.com>
2023-05-15 20:31:46 +0200

    Basic validation

--- frontend/src/calculator/bmrForm.tsx
+++ frontend/src/calculator/bmrForm.tsx
@@ -1,46 +1,46 @@
-import React, { FC, useState, FormEvent, ChangeEvent } from 'react';
+import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
 const BmrForm: FC = () => {
-    const [weight, setWeight] = useState<number | undefined>();
-    const [height, setHeight] = useState<number | undefined>();
-    const [age, setAge] = useState<number | undefined>();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    function handleSubmitting(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        console.log(weight, height, age);
-    }
-
-    function handleWeightChange(e: ChangeEvent<HTMLInputElement>) {
-        const newWeight = parseFloat(e.target.value);
-        setWeight(isNaN(newWeight) ? undefined : newWeight);
-    }
-
-    function handleHeightChange(e: ChangeEvent<HTMLInputElement>) {
-        const newHeight = parseFloat(e.target.value);
-        setHeight(isNaN(newHeight) ? undefined : newHeight);
-    }
-
-    function handleAgeChange(e: ChangeEvent<HTMLInputElement>) {
-        const newAge = parseFloat(e.target.value);
-        setAge(isNaN(newAge) ? undefined : newAge);
+    function handleSubmitting(formValues: Object) {
+        console.log(formValues);
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmitting}>
-                <section>
-                    <label>Enter your weight in kg</label>
-                    <input type="number" value={weight ?? ''} onChange={handleWeightChange} />
+        <div className="h-[50%]">
+            <form onSubmit={handleSubmit(handleSubmitting)} className='shadow-[0px_0px_30px_10px_#dd6b20] rounded-2xl p-12 flex flex-col align-middle h-full justify-around'>
+                <section className='flex flex-col items-center justify-center'>
+                    <label className='mb-2'>Enter your weight in kg</label>
+                    <input
+                        {...register("weight", { required: true, min: 0 })}
+                        className="appearance-none px-4 py-1 border border-gray-300 text-center max-w-xs rounded-lg"
+                    />
+                    {errors.weight && <span className="text-red-500 text-center">Weight is required and should be greater than or equal to 0</span>}
                 </section>
-                <section>
-                    <label>Enter your height in cm</label>
-                    <input type="number" value={height ?? ''} onChange={handleHeightChange} />
+                <section className='flex flex-col items-center justify-center'>
+                    <label className='mb-2'>Enter your height in cm</label>
+                    <input
+                        {...register("height", { required: true, min: 0 })}
+                        className="appearance-none px-4 py-1 border border-gray-300 text-center max-w-xs rounded-lg"
+                    />
+                    {errors.height && <span className="text-red-500 text-center">Height is required and should be greater than or equal to 0</span>}
                 </section>
-                <section>
-                    <label>Enter your age</label>
-                    <input type="number" value={age ?? ''} onChange={handleAgeChange} />
+                <section className='flex flex-col items-center justify-center'>
+                    <label className='mb-2'>Enter your age</label>
+                    <input
+                        {...register("age", { required: true, min: 0, max: 99 })}
+                        className="appearance-none px-4 py-1 border border-gray-300 text-center max-w-xs rounded-lg"
+                    />
+                    {errors.age && <span className="text-red-500 text-center">Age is required and should be between 0 and 99</span>}
                 </section>
-                <button type="submit">Submit</button>
+                <div className='flex flex-col items-center justify-center'>
+                    <button
+                        type="submit"
+                        className="max-w-m relative inline-flex items-center justify-center p-5 mt-5 overflow-hidden text-lg font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
+                        >Submit</button>
+                </div>
             </form>
         </div>
     );
