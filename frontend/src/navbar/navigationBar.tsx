09e1133b61120c54b18940ba6796873a08a10e17
Shershulia <krol31072002@gmail.com>
2023-06-07 15:15:22 +0200

    Style of navigation bar

--- frontend/src/navbar/navigationBar.tsx
+++ frontend/src/navbar/navigationBar.tsx
@@ -9,49 +9,54 @@ const NavigationBar = () => {
     };
 
     return (
-        <div >
-            <div className="flex fixed w-full z-20 top-0 left-0 border-b border-gray-200 justify-between items-center bg-orange-500 text-gray-900">
-                <h3 className="mx-4 py-4">Logo</h3> {/* Replace 'Logo' with your actual logo */}
+        <div>
+            <div className="flex fixed w-full z-20 top-0 left-0 border-b border-gray-200 justify-center items-center bg-orange-500 text-gray-900">
+                <div className="flex items-center justify-center">
+                    <img src="assets/TTGlogo.png" className="h-24 max-w-xs" onClick={editNavBar}/>
+                </div>
                 <nav
                     className={`flex flex-row justify-center items-center bg-orange-500 text-gray-900 ${
                         showNavBar ? 'block' : 'hidden'
                     } animate-fade-in`}
                 >
-                    <a className="mx-16 text-gray-900 px-16 py-4 hover:bg-orange-700" href="/#">
+                    <a className="mx-16 text-gray-900 px-16 py-9 hover:bg-orange-700" href="/#">
                         Home
                     </a>
-                    <a className="mx-16 text-gray-900 px-16 py-4 hover:bg-orange-700" href="/#">
+                    <a className="mx-16 text-gray-900 px-16 py-9 hover:bg-orange-700" href="/#">
                         Calculator
                     </a>
-                    <a className="mx-16 text-gray-900 px-16 py-4 hover:bg-orange-700" href="/#">
+                    <a className="mx-16 text-gray-900 px-16 py-9 hover:bg-orange-700" href="/#">
                         Exercises
                     </a>
-                    <a className="mx-16 text-gray-900 px-16 py-4 hover:bg-orange-700" href="/#">
+                    <a className="mx-16 text-gray-900 px-16 py-9 hover:bg-orange-700" href="/#">
                         My calories intake
                     </a>
                 </nav>
-                <button className={`p-4 ${!showNavBar ? 'hidden' : 'block'}`} onClick={editNavBar}>
-                    <FaTimes />
-                </button>
-                <button className={`p-4 ${showNavBar ? 'hidden' : 'block'}`} onClick={editNavBar}>
-                    <FaBars />
-                </button>
+                <div className='flex justify-end absolute right-0'>
+                    <button className={`p-4 ${!showNavBar ? 'hidden' : 'block'}`} onClick={editNavBar}>
+                        <FaTimes />
+                    </button>
+                    <button className={`p-4  ${showNavBar ? 'hidden' : 'block'}`} onClick={editNavBar}>
+                        <FaBars />
+                    </button>  
+                </div>
+                
             </div>
             <style>
                 {`
-                @keyframes fade-in {
-                  from {
-                    opacity: 0;
-                  }
-                  to {
-                    opacity: 1;
-                  }
-                }
-        
-                .animate-fade-in {
-                  animation: fade-in 0.5s ease-in-out;
-                }
-                `}
+        @keyframes fade-in {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+
+        .animate-fade-in {
+          animation: fade-in 0.5s ease-in-out;
+        }
+        `}
             </style>
         </div>
     );
